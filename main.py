9a7abf309f1fd575bddba13235ce5e0eb2af9d67
polina.novokreschenova <noverianhenon@yandex.ru>
2019-11-20 23:33:50 +0300

    add cli

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
-from apps import flask_app, custom_app, django_app
-from server import WSGIServer
+import click
+from typing import Any
 
-server = WSGIServer(host='localhost', port=8000)
-if __name__ == '__main__':
+from server import WSGIServer
+from apps import flask_app, custom_app, django_app
+
+
+apps = {
+    'django': django_app,
+    'flask': flask_app,
+    'custom': custom_app
+}
+
+
+@click.command()
+@click.option('--host', default='localhost')
+@click.option('--port', default=8000)
+@click.option('--app', required=True, type=click.Choice(apps.keys()))
+def main(host: str, port: int, app: Any) -> None:
+    server = WSGIServer(host=host, port=port)
     try:
-        server.run(django_app)
+        application = apps[app]
+        server.run(application)
     except KeyboardInterrupt:
         server.stop()
+
+
+if __name__ == '__main__':
+    main()
+
